075acd369ff4df616d68126291c2eb596a7279f3
mehmetdalmaz <mehmetdalmazzz@gmail.com>
2025-04-16 11:48:04 +0300

    cart manager oluşturuldu

--- Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Backend/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+604bda50cf7897acca6a8fa051b2b74cd3279044")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dab10d1b7bfb1660fd16e4195cdea0747ea70f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Backend/Services/Concrete/CartManager.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Data.Abstract;
+using Services.Abstract;
+
+namespace Services.Concrete
+{
+    public class CartManager : ICartService
+    {
+        private readonly ICartDal _cartDAl;
+        public CartManager(ICartDal cartDal)
+        {
+            _cartDAl = cartDal;
+        }
+
+
+        public void AddItem(Cart cart, Product product, int quantity)
+        {
+            var item = cart.CartItems.Where(c => c.ProductId == product.Id).FirstOrDefault();
+
+            if (item == null)
+            {
+                cart.CartItems.Add(new CartItem { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+        }
+
+        public void DeleteItem(Cart cart, int productId, int quantity)
+        {
+            var item = cart.CartItems.Where(c => c.ProductId == productId).FirstOrDefault();
+
+            if (item == null)
+            {
+                return;
+            }
+            item.Quantity -= quantity;
+            if (item.Quantity <= 0)
+            {
+                cart.CartItems.Remove(item);
+            }
+
+
+
+        }
+
+        public void TDelete(Cart t)
+        {
+            _cartDAl.Delete(t);
+        }
+
+        public Cart TGetByID(int id)
+        {
+            return _cartDAl.GetByID(id);
+        }
+
+        public List<Cart> TGetList()
+        {
+            return _cartDAl.GetList();
+        }
+
+        public void TInsert(Cart t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Cart t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
